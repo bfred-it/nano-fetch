9905f81a20c89e97c4e60d4bf808d3ac9997da80
Federico Brigante <github@bfred.it>
2017-08-27 17:00:52 +0700

    Shorten

--- index.js
+++ index.js
@@ -2,15 +2,13 @@
 export default (url, {responseType, method}) => new Promise(function (resolve, reject) {
 	const request = new XMLHttpRequest();
 	request.open(method || 'GET', url, true);
-	request.onerror = function (e) {
-		reject(new Error(e));
-	};
+	request.responseType = responseType || 'json';
+	request.onerror = reject;
 	request.onload = function () {
 		if (request.status >= 200 && request.status < 400) {
 			return resolve(request.response);
 		}
-		request.onerror(request.status);
+		reject(request.status);
 	};
-	request.responseType = responseType || 'json';
 	request.send();
 });
